c0b1b8960488b48277683e5307d1d76b32cce7af
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-23 00:04:47 +0900

    done : local-passport updated & done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const session = require("express-session");
-const passport = require("./config/passport");
+const passport = require("./passport/local-passport");
 
 const indexRouter = require("./routes");
 require("dotenv").config();
