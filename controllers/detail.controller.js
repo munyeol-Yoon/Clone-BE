767416967415940e78a410ecbea5c43cc5d3ecd4
mjmcode <mjm7542>
2023-07-24 22:56:15 +0900

    create : DetailPages POST API complete

--- controllers/detail.controller.js
+++ controllers/detail.controller.js
@@ -1,7 +1,63 @@
-const DetailService = require("../services/detail.service");
+const DetailService = require('../services/detail.service');
 
 class DetailController {
   detailService = new DetailService();
-}
 
+  // 집사진 생성
+  createDetail = async (req, res, next) => {
+    try {
+      const { userId } = res.locals.user
+      const { content, imgUrl, itemData, } = req.body;
+      const details = await this.detailService.createDetail(userId, content, imgUrl, itemData);
+
+      return res.status(201).json({ details });
+    } catch (error) {
+      if (error.status) return res.status(error.status).json({ errorMessage: error.message });
+      res.json({ errorMessage: error.message });
+    }
+  };
+
+  // 집사진 상세 조회
+  getDetailOne = async (req, res, next) => {
+    try {
+      const { detailsId } = req.params;
+      const [details, userData, itemData] = await this.detailService.findOneDetail(detailsId);
+
+
+      res.status(200).json({ details, userData, itemData });
+    } catch (error) {
+      if (error.status) return res.status(error.status).json({ errorMessage: error.message });
+      res.json({ errorMessage: error.message });
+    }
+  };
+
+  // 집사진 수정
+  updateDetail = async (req, res, next) => {
+    try {
+
+      const { detailsId } = req.params;
+      const { content, imgUrl, itemId } = req.body;
+      const details = await this.detailService.updateDetail(detailsId, content, imgUrl, itemId)
+
+      res.status(200).json({ Message: "집사진을 수정하였습니다" });
+    } catch (error) {
+      if (error.status)
+        return res.status(error.status).json({ errorMessage: error.message });
+      res.json({ errorMessage: error.message });
+    };
+  }
+
+  // 집사진 삭제
+  deleteDetail = async (req, res, next) => {
+    try {
+      const { detailsId } = req.params;
+      const details = await this.detailService.deleteDetail(detailsId)
+      res.status(200).json({ Message: "집사진을 삭제하였습니다" });
+    } catch (error) {
+      if (error.status)
+        return res.status(error.status).json({ errorMessage: error.message });
+      res.json({ errorMessage: error.message });
+    };
+  }
+}
 module.exports = DetailController;
--- routes/detail.js
+++ routes/detail.js
@@ -1,14 +1,14 @@
-const express = require("express");
-
-const DetailController = require("../controllers/detail.controller");
+const express = require('express');
+const authMiddleware = require("../middlewares/auth-middleware")
+const DetailController = require('../controllers/detail.controller');
 const detailController = new DetailController();
 
 const router = express.Router();
 
-router.get("/"); // 메인페이지
-router.post("/"); // 집사진 상세 생성
-router.get("/:detailId"); // 집사진 상세 조회
-router.put("/:detailId"); // 집사진 상세 수정
-router.delete("/:detailId"); // 집사진 상세 삭제
+router.get('/'); // 메인페이지
+router.post('/', authMiddleware, detailController.createDetail); // 집사진 상세 생성
+router.get('/:detailsId', detailController.getDetailOne); // 집사진 상세 조회
+router.put('/:detailsId', authMiddleware, detailController.updateDetail); // 집사진 상세 수정
+router.delete('/:detailsId', authMiddleware, detailController.deleteDetail); // 집사진 상세 삭제
 
 module.exports = router;
