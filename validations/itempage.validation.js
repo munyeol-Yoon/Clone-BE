8e8d46779417ed3070fd6e119e3399e7790a748f
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-25 16:53:35 +0900

    fix: item-validation reference fix

--- validations/itempage.validation.js
+++ validations/itempage.validation.js
@@ -1,13 +1,13 @@
-const joi = reiquire('joi');
+const joi = require("joi");
 
 // 상품페이지 > 상품정보
-const itemPageField = ['brandName', 'itemName', 'rating', 'discount', 'price'];
+const itemPageField = ["brandName", "itemName", "rating", "discount", "price"];
 
-const stringValidation = joi.string().not('').required().messages({
-  'string.base': '입력한 항목의 형식이 일치하지 않습니다.',
-  'string.empty': '모든 항목을 입력해 주세요.',
-  'any.not': '모든 항목을 입력해 주세요.',
-  'any.required': '모든 항목을 입력해 주세요.',
+const stringValidation = joi.string().not("").required().messages({
+  "string.base": "입력한 항목의 형식이 일치하지 않습니다.",
+  "string.empty": "모든 항목을 입력해 주세요.",
+  "any.not": "모든 항목을 입력해 주세요.",
+  "any.required": "모든 항목을 입력해 주세요.",
 });
 
 const itemPageModels = joi.object(
@@ -18,11 +18,11 @@ const itemPageModels = joi.object(
 );
 
 // 상품페이지 > 옵션
-const optionField = ['color', 'size'];
+const optionField = ["color", "size"];
 
-const optionValidation = joi.not('').required().messages({
-  'any.not': '옵션을 입력해 주세요.',
-  'any.required': '옵션을 입력해 주세요.',
+const optionValidation = joi.not("").required().messages({
+  "any.not": "옵션을 입력해 주세요.",
+  "any.required": "옵션을 입력해 주세요.",
 });
 
 const optionsModels = joi.object(
@@ -34,11 +34,11 @@ const optionsModels = joi.object(
 
 // 상품페이지 > 상품이미지
 const itemImgListsModels = joi.object({
-  itemImg: joi.string().not('').required().messages({
-    'string.base': '입력한 항목의 형식이 일치하지 않습니다.',
-    'string.empty': '상품 이미지를 등록해 주세요.',
-    'any.not': '상품 이미지를 등록해 주세요.',
-    'any.required': '상품 이미지를 등록해 주세요.',
+  itemImg: joi.string().not("").required().messages({
+    "string.base": "입력한 항목의 형식이 일치하지 않습니다.",
+    "string.empty": "상품 이미지를 등록해 주세요.",
+    "any.not": "상품 이미지를 등록해 주세요.",
+    "any.required": "상품 이미지를 등록해 주세요.",
   }),
 });
 
