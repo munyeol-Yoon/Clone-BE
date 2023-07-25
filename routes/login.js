e42ababa4f0d539c603e9bbe93efcfb637b99509
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-26 02:19:57 +0900

    update : redirect delete & json token body response

--- routes/login.js
+++ routes/login.js
@@ -23,7 +23,8 @@ router.get(
 
     // JWT를 쿠키에 담아 클라이언트에게 전달
     res.cookie("cowdog", `Bearer ${token}`);
-    res.redirect("https://flatland.shop/");
+    // res.redirect("https://flatland.shop/");
+    return res.status(200).json({ message: "카카오 로그인 성공", token });
   }
 );
 
