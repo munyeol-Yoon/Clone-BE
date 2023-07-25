3e7f0f1b07ade349c8245109aa1fb2ea16aee17a
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-25 12:33:23 +0900

    fix : register error-handleing added

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -3,6 +3,7 @@
 const RegisterService = require("../services/register.service");
 const { BadRequestError, ConflictError } = require("../utils/error.utils");
 
+const { registerValidation } = require("../validations/auth.validation");
 const bcrypt = require("bcrypt");
 
 class RegisterController {
@@ -10,11 +11,8 @@ class RegisterController {
 
   createUser = async (req, res, next) => {
     try {
-      const { email, nickname, password, confirm } = req.body;
-
-      if (password !== confirm) {
-        throw new BadRequestError("비밀번호가 틀립니다.");
-      }
+      const { email, nickname, password, confirm } =
+        await registerValidation.validateAsync(req.body);
 
       const salt = bcrypt.genSaltSync(6);
       const hashedPassword = bcrypt.hashSync(password, salt);
@@ -25,12 +23,10 @@ class RegisterController {
         hashedPassword
       );
 
-      if (!user) {
-        throw new ConflictError("이미 가입된 이메일 또는 닉네임입니다.");
-      }
-
       return res.status(201).json({ message: "회원가입을 완료하였습니다." });
     } catch (err) {
+      console.log(err);
+      if (err.isJoi) return res.status(409).json({ errorMessage: err.message });
       next(err);
     }
   };
